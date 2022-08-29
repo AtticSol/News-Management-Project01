26a49f4f26a466ce962975c1af7580cf783e2bde
Victoria Morozova <95623204+AtticSol@users.noreply.github.com>
2022-08-29 20:05:39 +0300

    Update UserDAOImpl.java

--- src/main/java/by/itac/project01/dao/impl/UserDAOImpl.java
+++ src/main/java/by/itac/project01/dao/impl/UserDAOImpl.java
@@ -20,12 +20,12 @@ public class UserDAOImpl implements UserDAO {
 	private static final Logger log = LogManager.getRootLogger();
 
 	private static final String ID_COLUMN = "id";
+	private static final String GET_ID_BY_LOGIN_SQL_REQUEST = "SELECT id FROM users WHERE login=?";
 
 	@Override
-	public int getID(String login) throws UserDAOException {
-		String selectIDByLoginSQLRequest = "SELECT id FROM users WHERE login=?";
+	public int getID(String login) throws UserDAOException { 
 		try (Connection con = ConnectionPool.getInstanceCP().takeConnection();
-				PreparedStatement ps = con.prepareStatement(selectIDByLoginSQLRequest)) {
+				PreparedStatement ps = con.prepareStatement(GET_ID_BY_LOGIN_SQL_REQUEST)) {
 
 			ps.setString(1, login);
 			ResultSet rs = ps.executeQuery();
@@ -40,13 +40,12 @@ public class UserDAOImpl implements UserDAO {
 
 	
 	private static final String ROLE_COLUMN = "role";
-
+	private static final String GET_ROLE_BY_ID_SQL_REQUEST = "SELECT role FROM users WHERE id=?";
+	
 	@Override
 	public String getRole(int userID) throws UserDAOException {
-
-		String selectRoleByUserIDSQLRequest = "SELECT role FROM users WHERE id=?";
 		try (Connection con = ConnectionPool.getInstanceCP().takeConnection();
-				PreparedStatement ps = con.prepareStatement(selectRoleByUserIDSQLRequest)) {
+				PreparedStatement ps = con.prepareStatement(GET_ROLE_BY_ID_SQL_REQUEST)) {
 
 			ps.setInt(1, userID);
 			ResultSet rs = ps.executeQuery();
@@ -60,16 +59,14 @@ public class UserDAOImpl implements UserDAO {
 
 	}
 
-
+	private static final String ADD_USER_SQL_REQUEST = "INSERT INTO users(login, password, role) VALUES(?,?,?)";
+	private static final String ADD_USER_DETAILS_SQL_REQUEST = "INSERT INTO user_details(users_id, name, email) VALUES(LAST_INSERT_ID(),?,?)";
+	
 	@Override
 	public boolean registration(NewUserInfo user) throws UserDAOException {
-
-		String addMainUserDataSQLRequest = "INSERT INTO users(login, password, role) VALUES(?,?,?)";
-		String addAdditionalUserDataSQLRequest = "INSERT INTO user_details(users_id, name, email) VALUES(LAST_INSERT_ID(),?,?)";
-
 		try (Connection con = ConnectionPool.getInstanceCP().takeConnection();
-				PreparedStatement ps1 = con.prepareStatement(addMainUserDataSQLRequest);
-				PreparedStatement ps2 = con.prepareStatement(addAdditionalUserDataSQLRequest)) {
+				PreparedStatement ps1 = con.prepareStatement(ADD_USER_SQL_REQUEST);
+				PreparedStatement ps2 = con.prepareStatement(ADD_USER_DETAILS_SQL_REQUEST)) {
 			
 			return registrationDataTransaction(ps1, ps2, con, user);
 
@@ -106,14 +103,14 @@ public class UserDAOImpl implements UserDAO {
 
 	
 	private static final String LOGIN_COLUMN = "login";
+	private static final String GET_LOGIN_SQL_REQUEST = "SELECT login FROM users";
 
 	@Override
 	public boolean isLogin(String login) throws UserDAOException {
 
-		String selectLoginSQLRequest = "SELECT login FROM users";
 		try (Connection con = ConnectionPool.getInstanceCP().takeConnection();
 				Statement st = con.createStatement();
-				ResultSet rs = st.executeQuery(selectLoginSQLRequest)) {
+				ResultSet rs = st.executeQuery(GET_LOGIN_SQL_REQUEST)) {
 
 			while (rs.next()) {
 				if ((rs.getString(LOGIN_COLUMN)).equals(login)) {
@@ -127,14 +124,14 @@ public class UserDAOImpl implements UserDAO {
 		return false;
 	}
 
+	
 	private static final String PASSWORD_COLUMN = "password";
+	private static final String GET_PASSWORD_SQL_REQUEST = "SELECT password FROM users WHERE login=?";
 
 	@Override
 	public boolean isCorrectPassword(String login, String password) throws UserDAOException {
-
-		String selectPasswordByLoginSQLRequest = "SELECT password FROM users WHERE login=?";
 		try (Connection con = ConnectionPool.getInstanceCP().takeConnection();
-				PreparedStatement ps = con.prepareStatement(selectPasswordByLoginSQLRequest)) {
+				PreparedStatement ps = con.prepareStatement(GET_PASSWORD_SQL_REQUEST)) {
 
 			ps.setString(1, login);
 			ResultSet rs = ps.executeQuery();
@@ -148,14 +145,13 @@ public class UserDAOImpl implements UserDAO {
 	}
 
 	private static final String EMAIL_COLUMN = "email";
+	private static final String GET_EMAIL_SQL_REQUEST = "SELECT * FROM user_details";
 
 	@Override
 	public boolean isEmail(String email) throws UserDAOException {
-
-		String selectEmailSQLRequest = "SELECT * FROM user_details";
 		try (Connection con = ConnectionPool.getInstanceCP().takeConnection();
-				PreparedStatement ps = con.prepareStatement(selectEmailSQLRequest);
-				ResultSet rs = ps.executeQuery(selectEmailSQLRequest)) {
+				PreparedStatement ps = con.prepareStatement(GET_EMAIL_SQL_REQUEST);
+				ResultSet rs = ps.executeQuery(GET_EMAIL_SQL_REQUEST)) {
 
 			while (rs.next()) {
 				if ((rs.getString(EMAIL_COLUMN)).equals(email)) {
